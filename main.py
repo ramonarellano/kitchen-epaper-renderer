afcb57d692c97d6db6643f855d96bfc5db189143
Ramon Arellano <ramon.arellon@akerbp.com>
2025-07-04 10:12:08 +0200

    Enhance weather display: show 12 two-hour periods for Oslo with icon, time, temp, precip, wind (side by side, half screen width)

--- main.py
+++ main.py
@@ -61,39 +61,64 @@ def get_events():
     ]
 
 def get_weather():
-    # yr.no compact JSON
-    url = f'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={YR_LAT}&lon={YR_LON}'
+    # Oslo, Norway coordinates
+    lat = 59.9139
+    lon = 10.7522
+    url = f'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={lat}&lon={lon}'
     headers = {'User-Agent':'kitchen-epaper/1.0'}
     data = requests.get(url, headers=headers).json()
-    # take first timeseries entry
-    ts = data['properties']['timeseries'][0]
-    temp = ts['data']['instant']['details']['air_temperature']
-    # pick icon by symbol_code
-    symbol = ts['data']['next_1_hours']['summary']['symbol_code' if 'next_1_hours' in ts['data'] else 'cloudy']
-    # Map yr.no symbol_code to our icon keys
-    if symbol.startswith('clearsky'):
-        icon = 'clear_night' if 'night' in symbol else 'sunny'
-    elif symbol.startswith('cloudy'):
-        icon = 'cloudy'
-    elif symbol.startswith('partlycloudy'):
-        icon = 'partly_cloudy'
-    elif symbol.startswith('fair'):
-        icon = 'sunny'
-    elif symbol.startswith('rain'):
-        icon = 'rain'
-    elif symbol.startswith('showers'):
-        icon = 'showers'
-    elif symbol.startswith('snow'):
-        icon = 'snow'
-    elif symbol.startswith('fog'):
-        icon = 'fog'
-    elif symbol.startswith('sleet'):
-        icon = 'sleet'
-    elif symbol.startswith('thunder'):
-        icon = 'thunder'
-    else:
-        icon = 'cloudy'
-    return f"{int(temp)}°C", icon
+    # Get 12 periods: every 2 hours for today
+    periods = list(range(0, 24, 2))  # 0, 2, ..., 22
+    now = datetime.datetime.utcnow()
+    times = []
+    for ts in data['properties']['timeseries']:
+        t = datetime.datetime.fromisoformat(ts['time'].replace('Z','+00:00'))
+        if t.date() == now.date() and t.hour in periods:
+            times.append(ts)
+            if len(times) == 12:
+                break
+    details = []
+    for ts in times:
+        t = datetime.datetime.fromisoformat(ts['time'].replace('Z','+00:00'))
+        hour = t.hour
+        temp = ts['data']['instant']['details']['air_temperature']
+        wind = ts['data']['instant']['details'].get('wind_speed', 0)
+        precip = 0
+        if 'next_1_hours' in ts['data']:
+            precip = ts['data']['next_1_hours']['details'].get('precipitation_amount', 0)
+            symbol = ts['data']['next_1_hours']['summary']['symbol_code']
+        else:
+            symbol = 'cloudy'
+        if symbol.startswith('clearsky'):
+            icon = 'clear_night' if 'night' in symbol else 'sunny'
+        elif symbol.startswith('cloudy'):
+            icon = 'cloudy'
+        elif symbol.startswith('partlycloudy'):
+            icon = 'partly_cloudy'
+        elif symbol.startswith('fair'):
+            icon = 'sunny'
+        elif symbol.startswith('rain'):
+            icon = 'rain'
+        elif symbol.startswith('showers'):
+            icon = 'showers'
+        elif symbol.startswith('snow'):
+            icon = 'snow'
+        elif symbol.startswith('fog'):
+            icon = 'fog'
+        elif symbol.startswith('sleet'):
+            icon = 'sleet'
+        elif symbol.startswith('thunder'):
+            icon = 'thunder'
+        else:
+            icon = 'cloudy'
+        details.append({
+            'hour': hour,
+            'temp': temp,
+            'wind': wind,
+            'precip': precip,
+            'icon': icon
+        })
+    return details
 
 def render_image(events, weather):
     # dimensions of your color e-paper
@@ -106,26 +131,59 @@ def render_image(events, weather):
     cal_w = int(W*0.6)
     draw.text((margin, margin), "Next 3-day Agenda:", font=font_header, fill=(0,0,0))
     y = margin + 30
-    block_h = 40
+    block_h = 32
+    max_days = 3
+    max_events_per_day = 4
+    # Group events by date
+    from collections import defaultdict
+    events_by_date = defaultdict(list)
     for date, summary in events:
-        # green block
+        events_by_date[date].append(summary)
+    # Only show up to max_days
+    for i, (date, summaries) in enumerate(sorted(events_by_date.items())[:max_days]):
+        # Date title
         draw.rectangle([margin, y, cal_w-margin, y+block_h], fill=(0,128,0))
-        # white text inside
-        draw.text((margin+5, y+8), f"{date}: {summary}",
-                  font=font_cal, fill=(255,255,255))
-        y += block_h + 5
-        if y + block_h > H*0.8: break
+        draw.text((margin+5, y+8), date, font=font_header, fill=(255,255,255))
+        y += block_h + 2
+        # Events for this day
+        for j, summary in enumerate(summaries[:max_events_per_day]):
+            draw.rectangle([margin+10, y, cal_w-margin-10, y+block_h], fill=(0,180,0))
+            draw.text((margin+15, y+8), summary, font=font_cal, fill=(255,255,255))
+            y += block_h + 2
+        # If too many events, show ellipsis
+        if len(summaries) > max_events_per_day:
+            draw.text((margin+15, y), "...", font=font_cal, fill=(0,0,0))
+            y += block_h//2
+        # Add extra space between days
+        y += 6
+        if y + block_h > H*0.8:
+            break
 
     # --- Right: Weather ---
-    wx_x = cal_w + margin
-    draw.text((wx_x, margin), "Today's Weather:", font=font_header, fill=(0,0,0))
-    wx_txt, wx_icon = weather
-    icon_img = weather_icons[wx_icon].resize((60,60), Image.Resampling.LANCZOS)
-    img.paste(icon_img, (wx_x, margin+40), icon_img)
-
-    # temperature in red
-    draw.text((wx_x+70, margin+50), wx_txt, font=font_weather, fill=(200,0,0))
-
+    wx_x = int(W*0.5) + margin
+    wx_w = int(W*0.5) - 2*margin
+    draw.text((wx_x, margin), "Oslo Weather Today:", font=font_header, fill=(0,0,0))
+    y_wx = margin + 30
+    row_h = int((H - y_wx - margin) / 12)
+    icon_size = min(row_h-4, 36)
+    for period in weather:
+        hour = period['hour']
+        temp = period['temp']
+        wind = period['wind']
+        precip = period['precip']
+        icon = period['icon']
+        icon_img = weather_icons[icon].resize((icon_size,icon_size), Image.Resampling.LANCZOS)
+        # Draw icon
+        img.paste(icon_img, (wx_x, y_wx), icon_img)
+        # Draw time
+        draw.text((wx_x+icon_size+8, y_wx+4), f"{hour:02d}:00", font=font_cal, fill=(0,0,0))
+        # Draw temp
+        draw.text((wx_x+icon_size+70, y_wx+4), f"{int(temp)}°C", font=font_weather, fill=(200,0,0))
+        # Draw precip
+        draw.text((wx_x+icon_size+140, y_wx+4), f"{precip:.1f}mm", font=font_cal, fill=(0,0,0))
+        # Draw wind
+        draw.text((wx_x+icon_size+210, y_wx+4), f"{wind:.1f}m/s", font=font_cal, fill=(0,0,0))
+        y_wx += row_h
     # return PNG bytes
     buf = io.BytesIO()
     img.save(buf, 'PNG')
